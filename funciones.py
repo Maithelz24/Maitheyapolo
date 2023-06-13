e243c371616bfa5dff03830dbe7038f138fda894
Maithe Florencia León Zúñiga <maitheflorencialeonzuniga@Air-de-Maithe.bbrouter>
2023-06-13 18:47:48 -0400

    cree la función para identificar si un número es primo

--- funciones.py
+++ funciones.py
@@ -1,3 +1,13 @@
-print("gatos")
-print("perros")
-print("apolo")
+def numero_primo(n):
+    if n == 1 or n == 0:
+        return False
+    elif n == 2:
+        return  True
+    elif n > 2:
+        for divisor in range(2, n):
+            if n % divisor == 0:
+                return False
+            elif n % divisor != 0 and divisor == n-1:
+                return True
+            
+print(numero_primo(7))

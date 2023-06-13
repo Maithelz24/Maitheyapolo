0b205c7e26ccc220777208f1da24dcc6496cd75c
Maithe Florencia León Zúñiga <maitheflorencialeonzuniga@Air-de-Maithe.bbrouter>
2023-06-13 19:32:09 -0400

    agregué la función para encontrar los números primos menores o iguales que el ingresado

--- funciones.py
+++ funciones.py
@@ -11,5 +11,20 @@ def numero_primo(n):
             
 print(numero_primo(7))
 
+def todos_los_primos(n):
+     primos = []
+     for m in range(2, n+1):
+          s_primo = True
+          for divisor in range(2, int(m ** 0.5) + 1):
+               if m % divisor == 0:
+                    s_primo = False
+                    break
+          if s_primo:
+               primos.append(m)
+     return primos
+print(todos_los_primos(20))
+
+                  
+
 
 
